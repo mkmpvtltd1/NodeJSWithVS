12129ad93622aa8b00d826957e9e6992d287c6e7
Manoj <mkmpvtltd@gmail.com>
2016-08-25 17:52:04 +0545

    Captcha integration in contact form Display captcha

--- Start/routes/contact.js
+++ Start/routes/contact.js
@@ -1,8 +1,14 @@
 ﻿
 exports.contact = function (req, res) {
-    res.render('contact', { title: 'Contact', year: new Date().getFullYear(), message: 'Your contact page' });
+    var captchapng = require('captchapng');
+    var p = new captchapng(80, 30, parseInt(Math.random() * 9000 + 1000)); // width,height,numeric captcha 
+    p.color(0, 0, 0, 0);  // First color: background (red, green, blue, alpha) 
+    p.color(80, 80, 80, 255); // Second color: paint (red, green, blue, alpha) 
+
+    var img = p.getBase64();
+    res.render('contact', { title: 'Contact', year: new Date().getFullYear(), message: '', captcha: img });
 };
-exports.contactSendMail = function (req, res) {
+exports.contact = function (req, res) {
     var config = require('config');
     var mailOptions = {
         from: '"' + config.get("Email.AdminName") + '👥" <' + config.get("Email.AdminEmail") + '>', // sender address 
@@ -15,15 +21,15 @@ exports.contactSendMail = function (req, res) {
     var transpostString = config.get("Email.Transporter");
     // create reusable transporter object using the default SMTP transport 
     var transporter = nodemailer.createTransport(transpostString);
-
+    res.render('contact', { captcha_error: 'error' });
     // send mail with defined transport object 
     transporter.sendMail(mailOptions, function (error, info) {
         if (error) {
-            res.render('contactSucess', { title: 'Contact', year: new Date().getFullYear(), message: 'Message Send to' + error });
+            res.render('contact', { title: 'Contact', year: new Date().getFullYear(), message: 'Message Send to' + error });
             console.log(error);
         }
         else {
-            res.render('contactSucess', { title: 'Contact', year: new Date().getFullYear(), message: 'Message Send to' + req.body.name });
+            res.render('contact', { title: 'Contact', year: new Date().getFullYear(), message: 'Message Send to' + req.body.name });
         }
         //console.log('Message sent: ' + info.response);
     });
